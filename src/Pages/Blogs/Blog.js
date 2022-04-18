cb31ce59f0068866286256c35e6579d0fb439b89
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-18 19:40:39 +0600

    answer the question in blog section

--- src/Pages/Blogs/Blog.js
+++ src/Pages/Blogs/Blog.js
@@ -12,16 +12,16 @@ const Blog = () => {
                <div>
                <strong>Answer:</strong>
                <strong>Authentication:</strong>
-                1.Authentication indicate who is the user.
-                2. Authentication is the first step to identifying a smart software system.
-                3. Authentication is visible and also visible by user.
+               <p> 1.Authentication indicate who is the user.</p>
+               <p>2. Authentication is the first step to identifying a smart software system.</p>
+                <p>3. Authentication is visible and also visible by user.</p>
                </div>
 
                 <div>
                <strong> Authorization:</strong>
-                1.Authorization determine the resources the user can identify.
-                2. Authorization take places after Authentication.
-                3. Authorization is not visible and also user can not change it.
+                <p>1.Authorization determine the resources the user can identify.</p>
+                <p>2. Authorization take places after Authentication.</p>
+                <p>3. Authorization is not visible and also user can not change it.</p>
                 </div>
             </p>
             </div>
@@ -30,15 +30,17 @@ const Blog = () => {
               <h4>2. Why we are  using firebase? What other options do we have to implement authentication?</h4>
               <p>
                   <div>
-                        1.Firebase is used many purpose that can help our apps to develop,grow and a quality app.
-                        2.Used to store  users data  like chat meassages,users details and other metadata.
-                        3.Used to send notification.
+                       <p> 1.Firebase is used many purpose that can help our apps to develop,grow and a quality app.</p>
+                        <p> 2.Used to store  users data  like chat meassages,users details and other metadata.</p>
+                        <p>3.Used to send notification.</p>
 
                   </div>
                   <div>
                     What other options do we have to implement authentication:
-                    1.web-config.
-                    2.parse.
+                    
+                    <p>Password Requirements</p> 
+                    <p>Transferring the Data</p> 
+                    <p>Hashing Passwords etc</p>
 
                   </div>
               </p>
@@ -46,11 +48,14 @@ const Blog = () => {
             <div className='second-question'>
                 <h4>  3. What other services does firebase provide other than authentication?  </h4>
                 <p>
-                    1.Cloud Firestore.
-                    2. Cloud Function.
-                    3.Hosting.
-                    4.Cloud Storage.
-                    5.Cloud Meassaging.       
+                   <p>1.Cloud Firestore.</p>
+                   <p>Parse:Open source backend platform</p>
+                   <p>Bcak4app:Parse hosting plateform</p>
+                   <p>Kinvey:Mobile Backend as a service</p>
+                   <p>Backendless:Mobile backend ans API services platform</p>
+                   <p>Pubnub:Real-time API and global meassages</p>
+                   <p>Kumulos:App performance management</p>
+                         
 
                 </p>
             </div>
